f3a4ef57287e4d138e2429870a939b8c2a892043
Nelson Daniel Durán <ndanield@hotmail.com>
2018-05-30 22:05:51 -0400

    Ruta y vista para agregar estudiantes lista

--- src/main/java/edu/pucmm/ce/Main.java
+++ src/main/java/edu/pucmm/ce/Main.java
@@ -7,15 +7,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class Main {
+    private static ArrayList<Estudiante> estudiantes = new ArrayList<>();
 
     public static void main(String[] args) {
         staticFileLocation("/public");
-
-        ArrayList<Estudiante> estudiantes = new ArrayList<>();
 
         estudiantes.add(new Estudiante(20132013, "Daniel", "Pichardo", "809-621-2328"));
         estudiantes.add(new Estudiante(20122013, "Pedro", "Caceres", "809-641-2428"));
@@ -26,6 +24,20 @@ public class Main {
             model.put("estudiantes", estudiantes);
             return new ModelAndView(model,"index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/show", (req, res) -> null);
+
+        get("/create", (req, res) -> new ModelAndView(null, "create.hbs"),
+                new HandlebarsTemplateEngine());
+
+        post("/create", (req, res) -> {
+            estudiantes.add(new Estudiante(Integer.parseInt(req.queryParams("matricula")),
+                    req.queryParams("nombre"),
+                    req.queryParams("apellido"),
+                    req.queryParams("telefono")));
+            res.redirect("/");
+            return null;
+        });
     }
 
 }
